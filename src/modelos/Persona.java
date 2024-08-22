53d8549728c6e8cb1acacaa7f099e14337fd4f12
Diego Baes <diegobaesv@gmail.com>
2024-08-21 20:56:25 -0500

    encapsulamiento

--- src/modelos/Persona.java
+++ src/modelos/Persona.java
@@ -2,11 +2,70 @@
 package modelos;
 
 public class Persona {
-    public String nombre;
-    public int edad;
-    public String apellido;
-    public String nacionalidad;
-    public int altura;
+    private String nombre;
+    private int edad;
+    private String apellido;
+    private String nacionalidad;
+    private int altura;
+    private int peso;
+    private float imc;
+    //getter y setter
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public float getImc() {
+        this.imc = peso/(altura*altura);
+        return imc;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+    
+   
+    
     
     public void cantar(){
         System.out.println("Hola soy "+nombre+" y estoy cantando!! EOO!!");
--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -8,7 +8,7 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("Programa iniciado!!");
         
-        Animal perro = new Animal();
+        /*Animal perro = new Animal();
         perro.edad = 3;
         perro.nombre = "Bobby";
         perro.sexo = "Macho";
@@ -19,17 +19,21 @@ public class Principal {
         gato.sexo = "Macho";
         
         perro.comer();
-        gato.correr();
+        gato.correr();*/
         
         //Crear una clase persona con atributos(5) y metodos(2), y crear 2 objetos a partir de ella
         //linea nueva
         Persona persona = new Persona();
-        persona.altura = 170;
-        persona.apellido = "Flores";
-        persona.nombre = "Juan";
-        persona.edad = 20;
-        persona.nacionalidad = "Peruano";
+        persona.setNombre("Juan");
+        persona.setApellido("Flores");
+        persona.setAltura(2);      
+        persona.setPeso(70);
+        persona.setEdad(20);
+        persona.setNacionalidad("Peruano");
+              
+        persona.cantar();        
         
-        persona.cantar();
+        System.out.println("La persona creada es: "+persona.getNombre());
+        System.out.println("El IMC es: "+persona.getImc());
     }
 }
